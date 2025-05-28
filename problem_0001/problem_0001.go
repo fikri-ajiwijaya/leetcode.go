3a2e6b42b925ac11c2c0b6150ec6871e0c4c3255
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-05-29 00:30:18 +0700

    Solve problem 0001

--- problem_0001/problem_0001.go
+++ problem_0001/problem_0001.go
@@ -5,5 +5,12 @@ func TwoSum(nums []int, target int) []int {
 }
 
 func twoSum(nums []int, target int) []int {
+	for i, n := range nums {
+		for j, m := range nums[i+1:] {
+			if n+m == target {
+				return []int{i, i + j + 1}
+			}
+		}
+	}
 	return nil
 }
