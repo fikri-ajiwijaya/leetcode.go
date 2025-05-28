afec89c8a48a83b3093ac0a0f3facfd971852cba
Fikri Ajiwijaya <fikri.ajiwijaya@gmail.com>
2025-05-29 02:07:17 +0700

    Solve problem 0002

--- problem_0002/problem_0002.go
+++ problem_0002/problem_0002.go
@@ -6,11 +6,18 @@ type ListNode struct {
 }
 
 func Slice_to_List(vals []int) *ListNode {
-	return nil
+	if len(vals) == 0 {
+		return nil
+	}
+	return &ListNode{vals[0], Slice_to_List(vals[1:])}
 }
 
 func List_to_Slice(l *ListNode) []int {
-	return nil
+	_l := []int{}
+	for node := l; node != nil; node = node.Next {
+		_l = append(_l, node.Val)
+	}
+	return _l
 }
 
 func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
@@ -18,5 +25,23 @@ func AddTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
 }
 
 func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	return nil
+	_l1 := List_to_Slice(l1)
+	_l2 := List_to_Slice(l2)
+	_l3 := []int{}
+	carry := 0
+	for i := 0; i < len(_l1) || i < len(_l2); i++ {
+		if i < len(_l1) {
+			carry += _l1[i]
+		}
+		if i < len(_l2) {
+			carry += _l2[i]
+		}
+		_l3 = append(_l3, carry%10)
+		carry /= 10
+	}
+	if carry != 0 {
+		_l3 = append(_l3, carry)
+	}
+	l3 := Slice_to_List(_l3)
+	return l3
 }
